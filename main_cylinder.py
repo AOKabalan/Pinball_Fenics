c3db0db6cc342ec29f3df1eb73dcab083fd6ddd5
AOKabalan <akabalan@sissa.it>
2024-07-17 17:46:19 +0200

    Add seperate functions for each method

---
+++ main_cylinder.py
@@ -0,0 +1,35 @@
+from dolfin import *
+from space import build_space
+from nstinc import solve_unsteady_navier_stokes_bdf3
+from nstinc import solve_unsteady_navier_stokes_theta
+from nstinc import solve_unsteady_navier_stokes_internet_theta
+from nstinc import solve_unsteady_navier_stokes_bdf2
+from nstinc import solve_unsteady_navier_stokes_bdf1
+
+"""Solve unsteady Navier-Stokes to resolve
+    Karman vortex street and save to file"""
+
+# Problem data
+
+
+
+u_in = Expression(("4.0*U*x[1]*(0.41 - x[1])/(0.41*0.41)", "0.0"),
+                    degree=2, U=1)
+nu = Constant(0.001)
+T = 8
+
+    # Discretization parameters
+N_circle = 16*2
+N_bulk = 64*2
+theta = 0.5
+dt = 0.05
+
+# Prepare function space, BCs and measure on circle
+W, bcs, ds_circle = build_space(N_circle, N_bulk, u_in)
+
+# Solve unsteady Navier-Stokes
+solve_unsteady_navier_stokes_bdf3(W, nu, bcs, T, dt, theta)
+#solve_unsteady_navier_stokes_theta(W, nu, bcs, T, dt, theta)
+#solve_unsteady_navier_stokes_internet_theta(W, nu, bcs, T, dt, theta)
+#solve_unsteady_navier_stokes_bdf2(W, nu, bcs, T, dt, theta)
+#solve_unsteady_navier_stokes_bdf1(W, nu, bcs, T, dt, theta)
--- nstinc.py
+++ nstinc.py
@@ -8,69 +8,66 @@ from matplotlib import colors
 import dolfin
 from dolfin import *
 
+def initialize_prob(dt,T,Re):
+
+    mesh = Mesh()
+    mvc = MeshValueCollection("size_t", mesh, mesh.topology().dim())
+    with XDMFFile("mesh/mesh.xdmf") as infile:
+        infile.read(mesh)
+        infile.read(mvc, "name_to_read")
+    cf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
+
+    mvc = MeshValueCollection("size_t", mesh, mesh.topology().dim()-1)
+    with XDMFFile("mesh/mf.xdmf") as infile:
+        infile.read(mvc, "name_to_read")
+    mf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
 
 
-mesh = Mesh()
-mvc = MeshValueCollection("size_t", mesh, mesh.topology().dim())
-with XDMFFile("mesh/mesh.xdmf") as infile:
-    infile.read(mesh)
-    infile.read(mvc, "name_to_read")
-cf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
-
-mvc = MeshValueCollection("size_t", mesh, mesh.topology().dim()-1)
-with XDMFFile("mesh/mf.xdmf") as infile:
-    infile.read(mvc, "name_to_read")
-mf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
+    cmap = colors.ListedColormap(['blue', 'yellow', 'green', 'red'])
+    idx = np.unique(mf.array())
+    #print(idx)
+    labels = dict()
 
 
-cmap = colors.ListedColormap(['blue', 'yellow', 'green', 'red'])
-idx = np.unique(mf.array())
-#print(idx)
-labels = dict()
+    # Define function spaces
+    # V_element = VectorFunctionSpace(mesh, 'P', 2)
+    # Q_element = FunctionSpace(mesh, 'P', 1)
+    # W_element = MixedElement([V_element, Q_element]) # Taylor-Hood
+    # W = FunctionSpace(mesh, W_element)
+
+    V_element = VectorElement("Lagrange", mesh.ufl_cell(), 2)
+    Q_element = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+    W_element = MixedElement(V_element, Q_element) # Taylor-Hood
+    W = FunctionSpace(mesh, W_element)
 
 
-# Define function spaces
-# V_element = VectorFunctionSpace(mesh, 'P', 2)
-# Q_element = FunctionSpace(mesh, 'P', 1)
-# W_element = MixedElement([V_element, Q_element]) # Taylor-Hood
-# W = FunctionSpace(mesh, W_element)
+    # Set parameter values
 
-V_element = VectorElement("Lagrange", mesh.ufl_cell(), 2)
-Q_element = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-W_element = MixedElement(V_element, Q_element) # Taylor-Hood
-W = FunctionSpace(mesh, W_element)
+    theta = 0.5 # Crank Nicolson
 
+    u_bar = 1.
+    # u_in = Expression(("1.5*u_bar*4/(0.41*0.41)*x[1]*(0.41 - x[1])", "0."), u_bar=u_bar, degree=2)
 
-# Set parameter values
-dt = 0.01
-T = 100  
-theta = 0.5 # Crank Nicolson
+    u_in = Constant((1., 0.))
 
+    rho = 1.0
+    diam = 1.5
+    nu = Constant(u_bar*diam*rho/Re) # obtained from the definition of Re = u_bar * diam / nu. In our case diam = 0.1.
+    mu = rho * nu
 
-Re = 40.
-u_bar = 1.
-# u_in = Expression(("1.5*u_bar*4/(0.41*0.41)*x[1]*(0.41 - x[1])", "0."), u_bar=u_bar, degree=2)
-
-u_in = Constant((1., 0.))
-
-rho = 1.0
-diam = 1.5
-nu = Constant(u_bar*diam*rho/Re) # obtained from the definition of Re = u_bar * diam / nu. In our case diam = 0.1.
-mu = rho * nu
-
-f = Constant((0., 0.))
-# Define inflow profile
+    f = Constant((0., 0.))
+    # Define inflow profile
 
 
-# Define boundary conditions
-bcu_inflow = DirichletBC(W.sub(0), u_in, mf, 1)
-bcu_walls = DirichletBC(W.sub(0), Constant((1.0, 0)), mf, 3)
-bcu_cylinder1 = DirichletBC(W.sub(0), Constant((0, 0)), mf,4)
-bcu_cylinder2 = DirichletBC(W.sub(0), Constant((0, 0)), mf,5)
-bcu_cylinder3 = DirichletBC(W.sub(0), Constant((0, 0)), mf,6)
-bcp_outflow = DirichletBC(W.sub(1), Constant(0), mf, 2)
-bcs = [bcu_inflow, bcu_walls, bcu_cylinder1, bcu_cylinder2, bcu_cylinder3]
-bcp = [bcp_outflow]
+    # Define boundary conditions
+    bcu_inflow = DirichletBC(W.sub(0), u_in, mf, 1)
+    bcu_walls = DirichletBC(W.sub(0), Constant((1.0, 0)), mf, 3)
+    bcu_cylinder1 = DirichletBC(W.sub(0), Constant((0, 0)), mf,4)
+    bcu_cylinder2 = DirichletBC(W.sub(0), Constant((0, 0)), mf,5)
+    bcu_cylinder3 = DirichletBC(W.sub(0), Constant((0, 0)), mf,6)
+    bcp_outflow = DirichletBC(W.sub(1), Constant(0), mf, 2)
+    bcs = [bcu_inflow, bcu_walls, bcu_cylinder1, bcu_cylinder2, bcu_cylinder3]
+    bcp = [bcp_outflow]
 
 
 
@@ -170,7 +167,7 @@ def solve_unsteady_navier_stokes_theta(W, nu, bcs, T, dt, theta):
     solver = NonlinearVariationalSolver(problem)
     solver.parameters['newton_solver']['linear_solver'] = 'mumps'
 
-    f = XDMFFile('velocity_unsteady_navier_stokes2.xdmf')
+    f = XDMFFile('velocity_unsteady_navier_stokes_theta1_method.xdmf')
     u, p = w.split()
 
     # Perform time-stepping
@@ -185,7 +182,7 @@ def solve_unsteady_navier_stokes_theta(W, nu, bcs, T, dt, theta):
 
 
 def solve_unsteady_navier_stokes_bdf3(W, nu, bcs, T, dt, theta):
-    """Solver unsteady Navier-Stokes and write results
+    """Solver unsteady Navier-Stokes with third order time integration and write results
     to file"""
 
     # Current and old solution
@@ -242,8 +239,8 @@ def solve_unsteady_navier_stokes_bdf3(W, nu, bcs, T, dt, theta):
     solver2 = NonlinearVariationalSolver(problem2)
     solver2.parameters['newton_solver']['linear_solver'] = 'mumps'
     
-    file1 = XDMFFile('results/velocity_unsteady_navier_stokes2.xdmf')
-    file2 = XDMFFile('results/pressure_unsteady_navier_stokes2.xdmf')
+    file1 = XDMFFile('results/velocity_unsteady_navier_stokes_bdf3.xdmf')
+    file2 = XDMFFile('results/pressure_unsteady_navier_stokes_bdf3.xdmf')
     u, p = w.split()
 
     # Perform time-stepping
@@ -275,11 +272,231 @@ def solve_unsteady_navier_stokes_bdf3(W, nu, bcs, T, dt, theta):
         print(f"Time step {t} completed")
         
 
-      
 
-# w = solve_stokes(W, nu, bcs)
-# save_and_plot(w, 'stokes')
+def solve_unsteady_navier_stokes_internet_theta(W, nu, bcs, T, dt, theta):
+    """Solver unsteady Navier-Stokes and write results
+    to file"""
 
-solve_unsteady_navier_stokes_bdf3(W, nu, bcs, T, dt, theta)
-# save_and_plot(w, 'navier_stokes_picard')
-# plt.show()
+    # Current and old solution
+    w = Function(W)
+    u, p = split(w)
+
+    w_old = Function(W)
+    u_old, p_old = split(w_old)
+
+    # Define variational forms
+    v, q = TestFunctions(W)
+    F = ( Constant(1/dt)*dot(u - u_old, v)
+          + Constant(theta)*nu*inner(grad(u), grad(v))
+          + Constant(theta)*dot(dot(grad(u), u), v)
+          + Constant(1-theta)*nu*inner(grad(u), grad(v))
+          + Constant(1-theta)*dot(dot(grad(u_old), u_old), v)
+          - p*div(v)
+          - q*div(u)
+        )*dx
+    J = derivative(F, w)
+
+    # Create solver
+    problem = NonlinearVariationalProblem(F, w, bcs, J)
+    solver = NonlinearVariationalSolver(problem)
+    solver.parameters['newton_solver']['linear_solver'] = 'mumps'
+
+    f = XDMFFile('velocity_unsteady_navier_stokes_internet_theta_method.xdmf')
+    u, p = w.split()
+
+    # Perform time-stepping
+    t = 0
+    while t < T:
+        w_old.vector()[:] = w.vector()
+        solver.solve()
+        t += dt
+        f.write(u, t)    
+
+
+
+
+def solve_unsteady_navier_stokes_bdf2(W, nu, bcs, T, dt, theta):
+    """Solver unsteady Navier-Stokes with third order time integration and write results
+    to file"""
+
+    # Current and old solution
+    w = Function(W)
+    u, p = split(w)
+
+    f = Constant((0., 0.))
+ 
+    w_1 = Function(W)
+    (u_1, _) = split(w_1)
+
+    w_2 = Function(W)
+    (u_2, _) = split(w_2)
+
+
+    # Define variational forms
+    v, q = TestFunctions(W)
+    F1 = (Constant(1/dt)*(Constant(1.5)*inner(u,v) - Constant(2.0)*inner(u_1,v) + Constant(0.5)*inner(u_2, v))*dx 
+      + nu*inner(grad(u), grad(v))*dx
+      + inner(grad(u)*u, v)*dx
+      - div(v)*p*dx
+      - div(u)*q*dx
+     
+    )
+ 
+    J1 = derivative(F1, w)
+
+
+    F2 = (   inner(u-u_1, v)/Constant(dt)*dx # Implit Euler discretization 
+        + nu*inner(grad(u), grad(v))*dx
+        + inner(grad(u)*u, v)*dx
+        - div(v)*p*dx
+        + div(u)*q*dx
+       
+        )
+
+    J2 = derivative(F2, w)
+
+    # Create solver
+    problem1 = NonlinearVariationalProblem(F1, w, bcs, J1)
+    solver1 = NonlinearVariationalSolver(problem1)
+    solver1.parameters['newton_solver']['linear_solver'] = 'mumps'
+
+    problem2 = NonlinearVariationalProblem(F2, w, bcs, J2)
+    solver2 = NonlinearVariationalSolver(problem2)
+    solver2.parameters['newton_solver']['linear_solver'] = 'mumps'
+    
+    file1 = XDMFFile('results/velocity_unsteady_navier_stokes_bdf2.xdmf')
+    file2 = XDMFFile('results/pressure_unsteady_navier_stokes_bdf2.xdmf')
+    u, p = w.split()
+
+    # Perform time-stepping
+    t = 0
+
+    solver2.solve()
+
+    t += dt
+
+    w_1.assign(w)
+
+    file1.write(u,t)
+
+    while t < T:
+        
+        
+        solver1.solve()
+        
+        w_2.assign(w_1)
+        w_1.assign(w)
+        
+        
+        t += dt
+        file1.write(u, t)
+        #file2.write(p, t)
+        print(f"Time step {t} completed")
+
+
+def solve_unsteady_navier_stokes_bdf1(W, nu, bcs, T, dt, theta):
+    """Solver unsteady Navier-Stokes with first order time integration and write results
+    to file"""
+
+    # Current and old solution
+    w = Function(W)
+    u, p = split(w)
+
+    f = Constant((0., 0.))
+ 
+    w_1 = Function(W)
+    (u_1, _) = split(w_1)
+    v, q = TestFunctions(W)
+  
+    # F = (   inner(u-u_1, v)/Constant(dt)*dx # Implit Euler discretization 
+    #     + nu*inner(grad(u), grad(v))*dx
+    #     + inner(grad(u)*u, v)*dx
+    #     - div(v)*p*dx
+    #     + div(u)*q*dx
+    #     - inner(f, v)*dx
+    #     )
+
+    F = (   inner(u,      v)/Constant(dt)*dx # Implit Euler discretization
+      - inner(u_1, v)/Constant(dt)*dx # Implit Euler discretization
+      + nu*inner(grad(u), grad(v))*dx
+      + inner(grad(u)*u, v)*dx
+      - div(v)*p*dx
+      + div(u)*q*dx
+      - inner(f, v)*dx
+    )
+ 
+
+    J = derivative(F, w)
+
+ 
+    problem = NonlinearVariationalProblem(F, w, bcs, J)
+    solver = NonlinearVariationalSolver(problem)
+    solver.parameters['newton_solver']['linear_solver'] = 'mumps'
+    
+    file1 = XDMFFile('results/velocity_unsteady_navier_stokes_bdf1.xdmf')
+    file2 = XDMFFile('results/pressure_unsteady_navier_stokes_bdf1.xdmf')
+    u, p = w.split()
+
+    # Perform time-stepping
+    t = 0
+
+   
+
+    while t < T:
+        
+        
+        solver.solve()
+        
+       
+        assign(w_1, w)
+        
+        file1.write(u, t)
+        t += dt
+        
+        #file2.write(p, t)
+        print(f"Time step {t} completed")
+
+
+def postprocess(w, nu, ds_circle):
+    """Return lift, drag and the pressure difference"""
+
+    u, p = w.split()
+
+    # Report drag and lift
+    n = FacetNormal(w.function_space().mesh())
+    force = -p*n + nu*dot(grad(u), n)
+    F_D = assemble(-force[0]*ds_circle)
+    F_L = assemble(-force[1]*ds_circle)
+
+    U_mean = 0.2
+    L = 0.1
+    C_D = 2/(U_mean**2*L)*F_D
+    C_L = 2/(U_mean**2*L)*F_L
+
+    # Report pressure difference
+    a_1 = Point(0.15, 0.2)
+    a_2 = Point(0.25, 0.2)
+    try:
+        p_diff = p(a_1) - p(a_2)
+    except RuntimeError:
+        p_diff = 0
+
+    return C_D, C_L, p_diff
+
+
+
+  
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    initialize_prob(0.01,100,80)
+    solve_unsteady_navier_stokes_bdf3(W, nu, bcs, T, dt, theta)
+    # save_and_plot(w, 'navier_stokes_picard')
+    # plt.show()
